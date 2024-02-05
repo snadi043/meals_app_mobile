a3cc02747066f4895204bea7a8767c7069588ad2
snadi043 <snadi043@uottawa.ca>
2024-02-05 13:21:27 -0500

    Wrapped the conainer with InkWell widget which makes the list-itmes tappable and also making the list items circular with border radius and splashcolor properties.

--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -11,23 +11,28 @@ class CategoryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            categoryModel.color.withOpacity(0.55),
-            categoryModel.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Text(
-        categoryModel.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                categoryModel.color.withOpacity(0.55),
+                categoryModel.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
+            borderRadius: BorderRadius.circular(16)),
+        child: Text(
+          categoryModel.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
